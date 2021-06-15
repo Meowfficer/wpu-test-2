20dc32c3c4f5732064b79784666307ab5ad74b1f
Meowfficer <alandraravi70@gmail.com>
2021-06-15 14:36:23 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -7,5 +7,10 @@
 <body>
 	<h1>Hello World</h1>
 	<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint non nihil optio cum doloremque expedita illo suscipit, saepe asperiores doloribus officia quo accusantium quasi ipsam. Odio ullam dolore vitae quidem.</p>
+	<ul>
+		<li>List 1</li>
+		<li>List 2</li>
+		<li>List 3</li>
+	</ul>
 </body>
 </html>
